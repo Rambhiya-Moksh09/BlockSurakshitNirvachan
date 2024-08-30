56f267a4e270fec53d986a17fc76b94d5d8d3f72
Moksh Rambhiya <rambhiyamoksh666@gmail.com>
2024-08-30 13:39:14 +0530

    Static Address comment

--- backend/web3.js
+++ backend/web3.js
@@ -8,7 +8,7 @@ export const web3 = new Web3("http://localhost:7545");
 //const provider = new Web3.providers.HttpProvider("http://localhost:7545");
 
 const contractABI = Election.abi;
-const contractAddress = process.env.Contract_Address;
+const contractAddress = process.env.Contract_Address; //Always changed the static address if truffle migrate --reset used
 //console.log(contractAddress)
 const ElectionContract = new web3.eth.Contract(contractABI,contractAddress)
 
