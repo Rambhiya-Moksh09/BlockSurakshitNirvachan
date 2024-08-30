2397b68b9a9234f0382dbf6017b5b492901ca706
Moksh Rambhiya <rambhiyamoksh666@gmail.com>
2024-08-30 12:49:23 +0530

    Web3 js config added

---
+++ backend/web3.js
@@ -0,0 +1,15 @@
+import Web3 from "web3";
+import dotenv from 'dotenv';
+
+import Election from "../Solidity/build/contracts/Election.json" assert {type :'json'}
+
+dotenv.config();
+export const web3 = new Web3("http://localhost:7545");
+//const provider = new Web3.providers.HttpProvider("http://localhost:7545");
+
+const contractABI = Election.abi;
+const contractAddress = process.env.Contract_Address;
+//console.log(contractAddress)
+const ElectionContract = new web3.eth.Contract(contractABI,contractAddress)
+
+export default ElectionContract;
