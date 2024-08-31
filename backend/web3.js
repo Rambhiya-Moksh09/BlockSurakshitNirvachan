273a1c462e3e48fcbe45d8c651b2570bfb224a4c
Moksh Rambhiya <rambhiyamoksh666@gmail.com>
2024-08-31 10:52:25 +0530

    Updated web3.js file

--- backend/web3.js
+++ backend/web3.js
@@ -1,7 +1,7 @@
 import Web3 from "web3";
 import dotenv from 'dotenv';
 
-import Election from "../Solidity/build/contracts/Election.json" assert {type :'json'}
+import Election from "../Solidity/build/contracts/Election.json" assert {type: 'json'}
 
 dotenv.config();
 export const web3 = new Web3("http://localhost:7545");
@@ -9,7 +9,5 @@ export const web3 = new Web3("http://localhost:7545");
 
 const contractABI = Election.abi;
 const contractAddress = process.env.Contract_Address; //Always changed the static address if truffle migrate --reset used
-//console.log(contractAddress)
-const ElectionContract = new web3.eth.Contract(contractABI,contractAddress)
 
-export default ElectionContract;
+export const ElectionContract = new web3.eth.Contract(contractABI, contractAddress)
