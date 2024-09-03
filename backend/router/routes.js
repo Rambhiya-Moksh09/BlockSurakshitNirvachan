c7e6d847602706bb0c774347ae763e41bfe06e8c
Moksh Rambhiya <rambhiyamoksh666@gmail.com>
2024-09-03 12:51:55 +0530

    Candidate routes added

--- backend/router/routes.js
+++ backend/router/routes.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 import { addNewAdmin, getAllAdmins } from '../controller/Admins/Admins.js'
 import { addVoterDetails, showVoterDetail } from '../controller/voters/voterData.js';
+import { addNewCandidate, getAllCandidates } from '../controller/candidate/candidates.js';
 
 const router = express.Router();
 
@@ -13,5 +14,9 @@ router.get('/getAdmins', getAllAdmins)
 //Voter routes
 router.post('/addVoter', addVoterDetails)
 router.get('/getVoters', showVoterDetail)
+
+//candidate routes
+router.post('/addCandidate', addNewCandidate)
+router.get('/getCandidates', getAllCandidates)
 
 export default router;
